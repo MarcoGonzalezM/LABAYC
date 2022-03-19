fc91872cd035a8f7c31313550f61398473b4fed0
MarcoGonzalezM <79273368+MarcoGonzalezM@users.noreply.github.com>
2022-03-19 16:59:56 +0100

    Casos Prueba
    Añadidos casos de prueba a inversiones.py

--- TEMA 3/inversiones.py
+++ TEMA 3/inversiones.py
@@ -17,4 +17,11 @@ def burbuja(c):
         r = burbuja(c[:-1])
         return r[0]+[c[-1]],r[1]+contador
 
-print(burbuja([9,1,3,4,2,10,6,5,8,7]))
+Cliente1 = [1, 2, 3, 4, 5, 6, 7, 8]
+Cliente2 = [6, 4, 3, 1, 8, 7, 2, 5]
+Cliente3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+Cliente4 = [7, 13, 2, 19, 10, 4, 9, 20, 1, 5, 15, 17, 6, 18, 3, 14, 16, 8, 12, 11]
+
+print(burbuja(Cliente2)[1])
+print(burbuja(Cliente4)[1])
+#print(burbuja([9,1,3,4,2,10,6,5,8,7]))
