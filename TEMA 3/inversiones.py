0b5096ee187b4313707090464053611db0b9fb95
MarcoGonzalezM <79273368+MarcoGonzalezM@users.noreply.github.com>
2022-03-19 16:22:07 +0100

    Añadir método burbuja

--- TEMA 3/inversiones.py
+++ TEMA 3/inversiones.py
@@ -4,17 +4,27 @@ def inversiones(preferencias1,preferencias2):
     
     """
 
-def mergesort(c):
+def burbuja(c):
     n = len(c)
     if n <=1:
         return c
-    
+    else:
+        for i in range(n-1):
+            if c[i]>c[i+1]:
+                c[i],c[i+1] = c[i+1],c[i]
+        return burbuja(c[:-1])+[c[-1]]
+
+
+def mergesort(c):
+    n = len(c)
+    if n <=1:
+        return c, 0
+
+    # División del problema en las dos mitades de la lista   
     a = mergesort(c[:n//2])
     b = mergesort(c[n//2:])
-    
     l = a[0]
     r = b[0]
-
     contador = a[1]+b[1]
 
     res = []
@@ -24,9 +34,11 @@ def mergesort(c):
             res.append(l[0])
             l = l[1:]
         else:
-            contador +=1
+            contador +=1             #En este caso al añadirse un elemento de la lista derecha, mientras quedaban elementos de la lista izquierda existe una inversión
             res.append(r[0])
             r = r[1:]
     # Restantes (l o r va a ser [])
     res += l + r
     return res, contador
+
+print(burbuja([9,1,3,4,2,10,6,5,8,7]))
