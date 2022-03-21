e41fb34014f16c9d4dce2b01d4021f492685256c
MarcoGonzalezM <79273368+MarcoGonzalezM@users.noreply.github.com>
2022-03-21 08:48:39 +0100

    Update inversiones.py
    Añadido método mergesort

--- TEMA 3/inversiones.py
+++ TEMA 3/inversiones.py
@@ -17,11 +17,40 @@ def burbuja(c):
         r = burbuja(c[:-1])
         return r[0]+[c[-1]],r[1]+contador
 
+def mergesort(c):
+    n = len(c)
+    if n <=1:
+        return c, 0
+    a = mergesort(c[:n//2])
+    b = mergesort(c[n//2:])
+    l = a[0]
+    r = b[0]
+    contador = a[1] + b[1]
+
+    res = []
+    while len(l) >0 and len(r) >0: 
+        if l[0] <= r[0]:
+            res.append(l[0])
+            l = l[1:]
+        else:
+            contador += len(l)
+            res.append(r[0])
+            r = r[1:]
+    # Restantes (l o r va a ser [])
+    res += l + r
+    return res, contador
+
+print(mergesort([1,90,52,26,7,2,46,78,3,7,12,74,26,78]))
+
 Cliente1 = [1, 2, 3, 4, 5, 6, 7, 8]
 Cliente2 = [6, 4, 3, 1, 8, 7, 2, 5]
 Cliente3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 Cliente4 = [7, 13, 2, 19, 10, 4, 9, 20, 1, 5, 15, 17, 6, 18, 3, 14, 16, 8, 12, 11]
 
-print(burbuja(Cliente2)[1])
+print(burbuja(Cliente2))
 print(burbuja(Cliente4)[1])
-#print(burbuja([9,1,3,4,2,10,6,5,8,7]))
+print(mergesort(Cliente2))
+print(mergesort(Cliente4)[1])
+
+print(mergesort([9,1,3,4,2,10,6,5,8,7]))
+print(mergesort([4,1,7,3,2,9,5]))
